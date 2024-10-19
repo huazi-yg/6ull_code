d7b64961c5e94f46bed19c054b5156bb7eec2fde
huazi <huazi_yg@163.com>
2024-10-19 21:56:26 +0800

    阻塞与非阻塞

--- key_dts/key_drv.c
+++ key_dts/key_drv.c
@@ -86,6 +86,11 @@ static ssize_t gpio_key_drv_read(struct file *file,char __user *buf,size_t size,
 	int err;
 	int key;
 
+	if(is_key_buf_empty() && file->f_flags & O_NONBLOCK)
+	{
+		return -EAGAIN;
+	}
+
 	wait_event_interruptible(gpio_key_wait, !is_key_buf_empty());
 	key = get_key();
 	err = copy_to_user(buf,&key,4);
--- key_dts/key_test.c
+++ key_dts/key_test.c
@@ -7,7 +7,7 @@
 #include <poll.h>
 #include <signal.h>
 
-
+//#define BLOCK
 int fd;
 
 static void sig_func(int sig)
@@ -35,7 +35,11 @@ int main(int argc,char **argv)
 	//注册信号函数
 	signal(SIGIO,sig_func);
 	//打开文件
+	#if BLOCK
 	fd = open(argv[1],O_RDWR);
+	#else
+	fd = open(argv[1],O_RDWR|O_NONBLOCK);
+	#endif
 	if(fd < 0)
 	{
 		printf("can not open %s\n",argv[1]);
@@ -43,17 +47,28 @@ int main(int argc,char **argv)
 	}
 
 	//把进程ID传递给驱动
-	fcntl(fd,F_SETOWN,getpid());
+	//fcntl(fd,F_SETOWN,getpid());
 
 	//使能驱动FASYNC
 	flags = fcntl(fd,F_GETFL);
-	fcntl(fd,F_SETFL,flags | FASYNC);
+	fcntl(fd,F_SETFL,flags );
 
+#if BLOCK
+	fcntl(fd,F_SETFL,flags & ~ O_NONBLOCK);
+#else
+	fcntl(fd,F_SETFL,flags | O_NONBLOCK);
+#endif
 
 	while(1)
 	{
-		printf("my_gpio_key\n");
-		sleep(2);
+		if(read(fd,&val,4) == 4)
+		{
+			printf("my_gpio_key 0x%x\n\n",val);
+		}
+		else
+		{
+			printf("while get button: -1\n");
+		}
 	}
 	close(fd);
 	return 0;
